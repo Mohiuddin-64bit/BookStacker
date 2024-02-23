b23a182ae5819e08379b93c8623f51518eaf4ca6
Mohiuddin <mohiuddin.niddu@gmail.com>
2024-02-24 01:39:21 +0600

    Add books data and BooksCard component

---
+++ src/data/books.js
@@ -0,0 +1,50 @@
+export const books = [
+  {
+   image: "http://example.com/image1.jpg",
+   title: "The Great Gatsby",
+   author: "F. Scott Fitzgerald",
+   page: 218,
+   date: "1925-04-10",
+   category: "Classic Literature"
+  },
+  {
+    image: "http://example.com/image2.jpg",
+    title: "1984",
+    author: "George Orwell",
+    page: 328,
+    date: "1949-06-08",
+    category: "Dystopian"
+  },
+  {
+    image: "http://example.com/image3.jpg",
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    page: 281,
+    date: "1960-07-11",
+    category: "Classic Literature"
+  },
+  {
+    image: "http://example.com/image4.jpg",
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    page: 234,
+    date: "1951-07-16",
+    category: "Classic Literature"
+  },
+  {
+    image: "http://example.com/image5.jpg",
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    page: 208,
+    date: "1988-04-15",
+    category: "Philosophical Fiction"
+  },
+  {
+    image: "http://example.com/image6.jpg",
+    title: "Sapiens: A Brief History of Humankind",
+    author: "Yuval Noah Harari",
+    page: 443,
+    date: "2011-01-01",
+    category: "Non-Fiction"
+  }
+]
